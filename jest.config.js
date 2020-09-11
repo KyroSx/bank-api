87dd3144926451cb414ca89606e7b3dde66a65c1
Leonardo <leolub150@gmail.com>
2020-09-11 19:01:44 -0300

    chore: update jest coverage

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,9 @@ module.exports = {
     '!<rootDir>/src/main/**',
     '!<rootDir>/src/**/*-protocols.ts',
     '!**/protocols/**',
-    '!**/test/**'
+    '!**/test/**',
+    '!<rootDir>/src/domain/**/*.ts',
+    '!<rootDir>/src/**/index.ts',
   ],
   coverageDirectory: "coverage",
   coverageProvider: "v8",
