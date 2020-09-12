d6f6bb7392ef47a67f8924816e4d4d585ea106cd
Leonardo <leolub150@gmail.com>
2020-09-12 18:03:27 -0300

    style: change app.use order

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -3,6 +3,6 @@ import { bodyParser, contentType, cors } from "../middlewares";
 
 export default (app: Express): void => {
   app.use(bodyParser);
-  app.use(contentType);
   app.use(cors);
+  app.use(contentType);
 };
