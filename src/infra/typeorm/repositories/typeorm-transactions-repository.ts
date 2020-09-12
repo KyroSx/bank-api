7842ceb3d7b1858a19cbe1a3ee665869d40e3f7a
Leonardo <leolub150@gmail.com>
2020-09-11 21:07:53 -0300

    feat: ensure TypeOrmTransactionsRepository add Transaction to database

--- src/infra/typeorm/repositories/typeorm-transactions-repository.ts
+++ src/infra/typeorm/repositories/typeorm-transactions-repository.ts
@@ -1,10 +1,23 @@
 import { Repository, getRepository } from "typeorm";
+import {
+  IAddTransactionRepository,
+  AddTransactionRepositoryParams,
+} from "@/data/protocols/repositories";
 import { Transaction } from "../entities";
 
-export class TypeOrmTransactionsRepository {
+export class TypeOrmTransactionsRepository
+  implements IAddTransactionRepository {
   private ormRepository: Repository<Transaction>;
 
   constructor() {
     this.ormRepository = getRepository("transactions");
+  }
+
+  async add(params: AddTransactionRepositoryParams) {
+    const transaction = this.ormRepository.create(params);
+
+    await this.ormRepository.save(transaction);
+
+    return transaction;
   }
 }
--- src/infra/typeorm/repositories/typeorm-transactions-respository.spec.ts
+++ src/infra/typeorm/repositories/typeorm-transactions-respository.spec.ts
@@ -1,10 +1,29 @@
-import { getConnection, createConnection } from "typeorm";
+import { getConnection, createConnection, getRepository } from "typeorm";
+import { mockTransactionModel } from "@/domain/tests";
 import { Category, Transaction } from "../entities";
 import { TypeOrmTransactionsRepository } from "./typeorm-transactions-repository";
 
 const makeSut = () => {
   const sut = new TypeOrmTransactionsRepository();
   return { sut };
+};
+
+const mockAddTransactionRepositoryParams = (category: Category) => {
+  const model = mockTransactionModel();
+  delete model.id;
+
+  model.category = category;
+
+  return model;
+};
+
+const insertCategory = async (): Promise<Category> => {
+  const repo = getRepository(Category);
+  const category = repo.create({ title: "any-title" });
+
+  await repo.save(category);
+
+  return category;
 };
 
 describe("Transactions Repository (Infra)", () => {
@@ -27,5 +46,18 @@ describe("Transactions Repository (Infra)", () => {
   it("should connect to in-memory database", () => {
     const { sut } = makeSut();
     expect(sut).toBeDefined();
+  });
+
+  it("should add Transaction to database", async () => {
+    const { sut } = makeSut();
+
+    const category = await insertCategory();
+    const params = mockAddTransactionRepositoryParams(category);
+
+    const transaction = await sut.add(params);
+
+    expect(transaction.id).toBeDefined();
+    expect(transaction.category).toEqual(category);
+    expect(transaction).toEqual(expect.objectContaining({ ...params }));
   });
 });
