f2ba9d319ca98a7f278c063a47a74841e0e155a8
Leonardo <leolub150@gmail.com>
2020-09-12 11:16:32 -0300

    feat: ensure ValidationComposite returns the first error if more then one validation fails

--- src/validation/validators/composite/validation-composite.spec.ts
+++ src/validation/validators/composite/validation-composite.spec.ts
@@ -2,7 +2,7 @@ import { MissingParamError } from "@/presentation/errors";
 import { makeValidationSpy } from "@/presentation/tests";
 import { ValidationComposite } from "./validation-composite";
 
-const field = "any_field";
+const field = "any-field";
 
 const makeSut = () => {
   const validationSpies = [makeValidationSpy(), makeValidationSpy()];
@@ -14,12 +14,23 @@ const makeSut = () => {
 };
 
 describe("Validation Composite", () => {
-  it("Should return an error if any validation fails", async () => {
+  it("should return an error if any validation fails", async () => {
     const { sut, validationSpies } = makeSut();
 
     validationSpies[1].error = new MissingParamError(field);
-    const error = await sut.validate({ [field]: "any_field" });
+    const error = await sut.validate({ [field]: field });
 
     expect(error).toEqual(validationSpies[1].error);
+  });
+
+  it("should return the first error if more then one validation fails", async () => {
+    const { sut, validationSpies } = makeSut();
+
+    validationSpies[0].error = new Error();
+    validationSpies[1].error = new MissingParamError(field);
+
+    const error = await sut.validate({ [field]: field });
+
+    expect(error).toEqual(validationSpies[0].error);
   });
 });
