4c31150b83ad867a628e91aa3be342ae381999e9
Leonardo <leolub150@gmail.com>
2020-09-12 17:52:14 -0300

    feat: update routes

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,12 +1,7 @@
 import { Express, Router } from "express";
-import { readdirSync } from "fs";
+import { transactionRoutes } from "../routes";
 
 export default (app: Express): void => {
   const router = Router();
-  app.use("/api", router);
-  readdirSync(`${__dirname}/../routes`).map(async (file) => {
-    if (!file.includes(".test.") && !file.endsWith(".map")) {
-      (await import(`../routes/${file}`)).default(router);
-    }
-  });
+  app.use("/api", transactionRoutes(router));
 };
--- src/main/routes/transactions-routes.ts
+++ src/main/routes/transactions-routes.ts
@@ -2,6 +2,9 @@ import { Router } from "express";
 import { adaptRoute } from "../adapters/express-route-adapter";
 import { makeAddTransactionController } from "../factories/controllers/transactions/make-add-transaction-controller";
 
-export default (router: Router): void => {
-  router.post("/transactions", adaptRoute(makeAddTransactionController()));
+export const transactionRoutes = (router: Router) => {
+  return router.post(
+    "/transactions",
+    adaptRoute(makeAddTransactionController()),
+  );
 };
