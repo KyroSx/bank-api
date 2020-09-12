7c0858640dfca69b2dd8d49941e756eacaf5a77a
Leonardo <leolub150@gmail.com>
2020-09-12 14:16:23 -0300

    feat: add AddTransactionValidation factory

--- src/main/factories/controllers/transactions/make-add-transactions-validation.ts
+++ src/main/factories/controllers/transactions/make-add-transactions-validation.ts
@@ -1,5 +1,21 @@
 import { IValidation } from "@/presentation/protocols";
+import { ValidationComposite } from "@/validation/composite/validation-composite";
+import {
+  OverLimitFieldValidation,
+  RequiredFieldValidation,
+} from "@/validation/validators";
 
-export const makeAddTransactionValidation = () => {
-  return {} as IValidation;
+export const makeAddTransactionValidation = (): IValidation => {
+  const validations: IValidation[] = [];
+  const requiredFields = ["title", "value", "type", "category"];
+
+  for (const field of requiredFields) {
+    validations.push(new RequiredFieldValidation(field));
+  }
+
+  validations.push(new OverLimitFieldValidation("type", 0));
+
+  const addTransactionValidation = new ValidationComposite(validations);
+
+  return addTransactionValidation;
 };
