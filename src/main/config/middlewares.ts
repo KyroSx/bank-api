33f36194f49d4a35beaade58118254583f3a5a61
Leonardo <leolub150@gmail.com>
2020-09-12 15:05:11 -0300

    feat: add cors middleware

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,7 +1,8 @@
 import { Express } from "express";
-import { bodyParser, contentType } from "../middlewares";
+import { bodyParser, contentType, cors } from "../middlewares";
 
 export default (app: Express): void => {
   app.use(bodyParser);
   app.use(contentType);
+  app.use(cors);
 };
