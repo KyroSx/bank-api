bd6f2cd08ee06ea7f20c5f06b3a0a6076856dd63
Leonardo <leolub150@gmail.com>
2020-09-13 18:13:34 -0300

    feat: add GET transactions route

--- src/main/routes/transactions-routes.ts
+++ src/main/routes/transactions-routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { adaptRoute } from "../adapters/express-route-adapter";
 import { makeAddTransactionController } from "../factories/controllers/transactions";
+import { makeFetchTransactionsController } from "../factories/controllers/transactions/fetch-transactions/make-fetch-transactions-controller";
 
-export const transactionRoutes = (router: Router) => {
-  return router.post(
-    "/transactions",
-    adaptRoute(makeAddTransactionController()),
-  );
+export const transactionRoutes = (router: Router): Router => {
+  router.post("/transactions", adaptRoute(makeAddTransactionController()));
+
+  router.get("/transactions", adaptRoute(makeFetchTransactionsController()));
+
+  return router;
 };
